b0f572a05fd0617f43407ef49f4b83ed9c4bfde1
HannahTa <hannah.taylor@tesco.net>
2016-05-22 14:32:15 +0100

    Planning - First Push
    This is the first push of all the files which were used in the start of
    the planning stage.

---
+++ UntitledAdventure/UntitledAdventure/Game1.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace UntitledAdventure
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteFont font;
+
+        GameStates state;
+        enum GameStates
+        {
+            Menu,
+            Loading,
+            Playing,
+            Paused,
+            Map
+        };
+
+        int sW = 800; // Get Screen Width!!
+        int sH = 600; // Get Screen Height
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            state = GameStates.Menu;
+
+            graphics.PreferredBackBufferHeight = sH;
+            graphics.PreferredBackBufferWidth = sW;
+            graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            
+            font = Content.Load<SpriteFont>("my_font");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            // Draw shit in here!!
+            // Typical Order:
+            //  1. Background/Screen            - Done in this class
+            //  2. Objects/Enemies              - Classes store in this class array
+            //  3. Player/Character             - Own object, stored in variable
+            //  4. UI/Map/Pause Menu/etc...     - Set, done in this class (Method perhaps)
+
+            spriteBatch.DrawString(font, "Boobies", new Vector2((sW / 2), (sH / 2)), Color.GhostWhite);
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
